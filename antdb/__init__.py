f0e6fc03559c7a537d883d87307b86c4d9bd7d04
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2025-04-18 17:06:09 -0300

    🚚 refactor: reorganize antdb module structure and add type hints
    Restructure the antdb module by moving `__init__.py` and `sgbd.py` to the root directory. Add type hints and noqa comments to improve code quality and maintainability. The changes ensure better organization and adherence to type checking standards.

---
+++ antdb/__init__.py
@@ -0,0 +1,3 @@
+from .sgbd import DataBase as DataBase  # noqa
+from .sgbd import Table as Table  # noqa
+from .sgbd import TableModel as TableModel  # noqa
--- src/antdb/__init__.py
+++
@@ -1,3 +0,0 @@
-from .sgbd import DataBase as DataBase
-from .sgbd import Table as Table
-from .sgbd import TableModel as TableModel
